12755eb046f6e471e678900fa5b1328be1651dd1
yuwensong <719629214@qq.com>
2016-04-13 13:53:06 +0800

    ok

--- config.php
+++ config.php
@@ -3,6 +3,6 @@
 define ("TOTAL_PAGE", 37);
 
 $link = mysqli_connect("localhost", "root", "root", "blog") or die("小样链接数据库失败!");
-mysqli_query( $link, " character_set_server = 'utf8'");
+mysqli_query( $link, "set names utf8");	//设置编码
 
 ?>
--- index.php
+++ index.php
@@ -6,9 +6,10 @@ require './common.php';	//公共函数文件
 use QL\QueryList;
 
 //1.先获取列表数据并插入到records表中
-function insertListData(){
-	$page = 1;
-	while($page < TOTAL_PAGE){
+function insertListData($page){
+	global $link;
+	//$page = 1;
+	//while($page < TOTAL_PAGE){
 		$listData = getListData($page);
 		//print_r($listData);die;
 		if(!empty($listData)){
@@ -20,30 +21,33 @@ function insertListData(){
 				if(!$res->num_rows){
 					$sql = "insert into records(url, title) values('".$v['url']."','".$v['title']."')";
 					mysqli_query($link, $sql);
+					
+					insertActicle($v['url']);
 				}
 			}
 		}
-		$page++;
-	}
+	//	$page++;
+	//}
 }
 
-
-
 //2.插入acticle表中
-//获取单条记录的数据并插入到acrticle表中
-function insertActicle(){
-	$data = getDetialData($v['url']);
+//获取单条记录的数据并插入到acticle表中
+function insertActicle($url){
+	global $link;
+	
+	$data = getDetialData($url);
+	//print_r($data);die;
 	if(!empty($data)){
 		foreach($data as $vv){
-			$tag = !empty($vv['tag'])?$vv['tag']:"";
-			
 			$sqlA = '';
-			$sqlA .= "insert into records(title, content, type, tag, crdate, readcount)";
-			$sqlA .= " values('".$vv['title']."','".$vv['content']."','".$vv['type'];
-			$sqlA .= "','".$tag."','";
-			$sqlA .= !empty($vv['crdate'])?strtotime($vv['crdate']):0;
-			$sqlA .= "','".!empty($vv['readCount'])?$vv['readCount']:"0";
-			$sqlA .= "', )";
+			$sqlA .= "insert into acticle(title, content, type, crdate, readcount)";
+			$sqlA .= " values('".(!empty($vv['title'])?$vv['title']:" ")."','".$vv['content'];
+			$sqlA .= "','".(!empty($vv['type'])?$vv['type']:" ");
+			$sqlA .= "',"."'";
+			$sqlA .= (!empty($vv['crdate'])?$vv['crdate']:0);
+			$sqlA .= "','".(!empty($vv['readCount'])?$vv['readCount']:"0");
+			$sqlA .= "')";
+
 			mysqli_query($link, $sqlA);
 		}
 	}
@@ -59,7 +63,7 @@ function getListData($page=1){
 		"title"=>array("#mainContent>.forFlow>.day>.postTitle>a", "text")
 	);
 
-	$html = file_get_contents($listUrl);
+	$html = @file_get_contents($listUrl);
 	if(empty($html)){
 		return ;
 	}
@@ -75,14 +79,28 @@ function getDetialData($url){
 	//$url = "http://www.cnblogs.com/yuwensong/p/5370151.html";
 	$rules = array(
 		"title"=>array("#topics>.post>.postTitle>a", "text"), //标题
-		"content"=>array("#cnblogs_post_body", "html"), //内容
-		"type"=>array("#BlogPostCategory", "text"), //分类
-		"tag"=>array("#EntryTag>a", "text"),	//标签
-		"date"=>array("#post-date", "text"),	//日期
+		"content"=>array("#cnblogs_post_body", "html", '', function($content){
+			$doc = phpQuery::newDocumentHTML($content); 
+			$imgs = pq($doc)->find('img'); 
+			foreach ($imgs as $img) { 
+				$src = pq($img)->attr('src');
+				$localSrc = 'upload/'.md5($src).'.png'; 
+				$stream = file_get_contents($src);
+				file_put_contents($localSrc,$stream); 
+				pq($img)->attr('src',$localSrc); 
+			} 
+			return $doc->htmlOuter();
+		}), //内容
+		"type"=>array("#BlogPostCategory", "text", '', function($content){
+			return empty($content)?" ":$content;
+		}), //分类
+		"date"=>array("#post-date", "text", '', function($content){
+			return strtotime($content);
+		}),	//日期
 		"readCount"=>array("#post_view_count", "html")	//阅读次数
 	);
 	
-	$html = file_get_contents($url);
+	$html = @file_get_contents($url);
 	$data = QueryList::Query($html, $rules, '', "UTF-8", "UTF-8")->data;
 	
 	return $data;
@@ -91,9 +109,39 @@ function getDetialData($url){
 
 
 ?>
+<?php
 
+//在浏览器中打开，配合自动提交，解决30秒钟超时问题
+$page = empty($_GET['page'])?"1":$_GET['page'];
+$page ++;
+file_put_contents("1.log", $page);
+if($page > TOTAL_PAGE){
+	echo "this is over";
+	die;
+}
 
+//调用获取列表的数据
+insertListData($page);
 
+?>
+<html>
+	<head>
+	<script src="./jquery-2.1.3.min.js"></script>		
+	</head>
+	<body>
+	<h1>this is a test</h1>
+		<form id="sub" action="./index.php?page=<?php echo $page;?>">
+			<input type="text" name="page"
+	 value="<?php echo $page;?>" />
+			<input type="submit"   value="submit" />
+		</form>
+	</body>
+	<script>
+		$(function(){
+			$("#sub").submit();
+		});	
+	</script>
+</html>
 	
 
 
